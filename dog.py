35875b80b20c66f634ee07b798affaa8952e40c4
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2020-01-04 11:47:44 +0100

    Handle situation when sensor is down

--- dog.py
+++ dog.py
@@ -1,5 +1,7 @@
-import datadog
+import logging
 import os
+
+import datadog
 
 datadog.initialize(api_key=os.environ['DD_API_KEY'],
                    app_key=os.environ['DD_APP_KEY'])
@@ -9,6 +11,9 @@ stats.start()
 
 
 def send(measurement, installation_id):
+    if len(measurement['values']) == 0:
+        logging.warning("Sensor seems to be down...")
+        return
     pm25 = next(x['value'] for x in measurement['values'] if x['name'] == 'PM25')
     pm10 = next(x['value'] for x in measurement['values'] if x['name'] == 'PM10')
     stats.gauge('air.airly.pm25', pm25, tags=[f"installationId:{installation_id}"])
